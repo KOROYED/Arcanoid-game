274f01bd5c4db8a229d050af3993750d14e04035
KOROYED <koroyed3883@gmail.com>
2021-09-07 15:37:23 +0300

    Blocks and restart system
    Added blocks to destroy and ball count based restart system.

--- Arkanoid/Assets/Scripts/Ball.cs
+++ Arkanoid/Assets/Scripts/Ball.cs
@@ -6,10 +6,22 @@ public class Ball : MonoBehaviour
 {
     public float speed = 100.0f;
     private Rigidbody2D rigidbody2D;
+    float visibleHeightThreshold;
+    public GameObject Player;
+
+    private bool IsGameStarted = false;
+    public float ballCount;
+
+
     void Start()
     {
+        visibleHeightThreshold = -Camera.main.orthographicSize - transform.localScale.y;
         rigidbody2D = transform.GetComponent<Rigidbody2D>();
-        rigidbody2D.velocity = Vector2.up * speed;
+
+    }
+    private void Update()
+    {
+        RestartSystem();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -22,9 +34,39 @@ public class Ball : MonoBehaviour
 
             rigidbody2D.velocity = dir * speed;
         }
+        if(collision.gameObject.tag == "Block")
+        {
+            Destroy(collision.gameObject);
+        }
     }
     float HitFactor (Vector2 ballPos, Vector2 playerPos, float playerWidth)
     {
         return (ballPos.x - playerPos.x) / playerWidth;
+    }
+
+    void RestartSystem()
+    {
+        if (IsGameStarted == false)
+        {
+            transform.position = new Vector2(Player.transform.position.x, Player.transform.position.y + 0.3f);
+        }
+        if (IsGameStarted == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.velocity = Vector2.up * speed;
+            IsGameStarted = true;
+
+        }
+        if (transform.position.y < visibleHeightThreshold)
+        {
+            transform.position = new Vector2(Player.transform.position.x, Player.transform.position.y + 0.3f);
+            rigidbody2D.velocity = Vector2.zero;
+            ballCount--;
+            IsGameStarted = false;
+        }
+        if (ballCount == 0)
+        {
+            Destroy(gameObject);
+            print("You lost");
+        }
     }
 }
