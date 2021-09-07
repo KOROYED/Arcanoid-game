3e061aaf98dc4a19ac99bc231df33f8f5739de9d
KOROYED <koroyed3883@gmail.com>
2021-09-07 13:38:26 +0300

    Project start
    Created basic background and implemented player movement

---
+++ Arkanoid/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    float screenHalfWidthInWorldUnits;
+
+    void Start()
+    {
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
+    }
+
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float velocity = inputX * speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+        float halfPlayerWidth = transform.localScale.x / 2;
+
+
+        if (transform.position.x < -screenHalfWidthInWorldUnits + halfPlayerWidth)
+        {
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits + halfPlayerWidth, transform.position.y);
+        }
+        if (transform.position.x > screenHalfWidthInWorldUnits - halfPlayerWidth)
+        {
+            transform.position = new Vector2(screenHalfWidthInWorldUnits - halfPlayerWidth, transform.position.y);
+        }
+    }
+}
